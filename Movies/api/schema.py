f7accdec79b04e3205409d503ffb14953b5b06b8
ElijahCoding <cuiyeqing960904@gmail.com>
2019-10-14 15:51:43 +0300

    relay data app

--- Movies/api/schema.py
+++ Movies/api/schema.py
@@ -1,7 +1,9 @@
 import graphene
+from graphene import relay
 import graphql_jwt
 from graphql_jwt.decorators import login_required
 from graphene_django.types import DjangoObjectType
+from graphene_django.filter import DjangoFilterConnectionField
 from .models import Director, Movie
 
 class MovieType(DjangoObjectType):
@@ -17,27 +19,41 @@ class DirectorType(DjangoObjectType):
     class Meta:
         model = Director
 
-class Query(graphene.ObjectType):
-    all_movies = graphene.List(MovieType)
-    movie = graphene.Field(MovieType, id=graphene.Int(), title=graphene.String())
 
+# Relay Implementation
+class MovieNode(DjangoObjectType):
+    class Meta:
+        model = Movie
+        filter_fields = {
+            'title': ['exact', 'icontains', 'istartswith'],
+            'year': ['exact', ]
+        }
+        interfaces = (relay.Node,)
+    
+    
+class Query(graphene.ObjectType):
+    # all_movies = graphene.List(MovieType)
+    # movie = graphene.Field(MovieType, id=graphene.Int(), title=graphene.String())
+    all_movies = DjangoFilterConnectionField(MovieNode)
+    movie = relay.Node.Field(MovieNode)
+    
     all_directors = graphene.List(DirectorType)
     
-    @login_required
-    def resolve_all_movies(self, info, **kwargs):
-        return Movie.objects.all()
+    # @login_required
+    # def resolve_all_movies(self, info, **kwargs):
+    #     return Movie.objects.all()
 
-    def resolve_movie(self, info, **kwargs):
-        id = kwargs.get('id')
-        title = kwargs.get('title')
-
-        if id is not None:
-            return Movie.objects.get(pk=id)
-
-        if title is not None:
-            return Movie.objects.get(title=title)
-
-        return None
+    # def resolve_movie(self, info, **kwargs):
+    #     id = kwargs.get('id')
+    #     title = kwargs.get('title')
+    # 
+    #     if id is not None:
+    #         return Movie.objects.get(pk=id)
+    # 
+    #     if title is not None:
+    #         return Movie.objects.get(title=title)
+    # 
+    #     return None
 
     def resolve_all_directors(self, info, **kwargs):
         return Director.objects.all()
