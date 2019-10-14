8ccb8c9241df4df47cb93b6b542aecc324be1644
ElijahCoding <cuiyeqing960904@gmail.com>
2019-10-14 11:38:15 +0300

    name problem fixed

--- Movies/wsgi.py
+++ Movies/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Movides project.
+WSGI config for Movies project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Movides.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Movies.settings')
 
 application = get_wsgi_application()
